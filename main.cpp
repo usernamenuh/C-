8ffd463a2baf2a89fd67a981c5006f19b2bf0c32
Usernamenuh <muhammadenuh4@gmail.com>
2025-07-09 01:56:55 +0700

    Tahap 4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Deklarasi menu dari file terpisah
 void menuTokoBuah();    // Dari tugas 1 - Linked List
 void menuTree();        // Dari tugas 2 - Tree
-void menuGraph(); // <-- deklarasi
+void menuGraph();
 
 int main() {
     // Identitas mahasiswa
